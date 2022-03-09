1136ff908da7227a9e880834fd81de4467b71731
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-03-09 20:21:54 +0100

    minor fixes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,16 +1,25 @@
 import classes from "./Footer.module.css";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaTripadvisor } from "react-icons/fa";
-import { FaInstagramSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import { FaCopyright } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
 function Footer() {
   return (
     <footer className={classes.footer}>
       <div>
-        <FaFacebookSquare />
-        <FaTripadvisor />
-        <FaInstagramSquare />
+        <IconContext.Provider value={{ className: classes.icon }}>
+          <a href="https://www.facebook.it/">
+            <FaFacebookSquare />
+          </a>
+          <a href="https://www.tripadvisor.it/">
+            <FaTripadvisor />
+          </a>
+          <a href="https://www.instagram.com/">
+            <FaInstagram />
+          </a>
+        </IconContext.Provider>
       </div>
       <div>
         <p>
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,20 @@
 import classes from "./Header.module.css";
 import logo from "../../images/logo.png";
-import ForkButton from "../ui/ForkButton";
+import { GiKnifeFork } from "react-icons/gi";
+import { IconContext } from "react-icons/lib";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className={classes.container}>
-      <img src={logo} alt="logo bounty pub" className={classes.logo} />
-      <ForkButton />
+      <Link to="/">
+        <img src={logo} alt="logo bounty pub" className={classes.logo} />
+      </Link>
+      <Link to="/menu">
+        <IconContext.Provider value={{ className: classes.icon }}>
+          <GiKnifeFork />
+        </IconContext.Provider>
+      </Link>
     </header>
   );
 }
