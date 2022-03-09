d7335d3b3a1c5d0864b378885abdf724ad76b6e7
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2022-03-09 22:25:33 +0100

    fix headerr and footer width

--- src/components/ui/ForkButton.jsx
+++
@@ -1,8 +0,0 @@
-import logo from "../../images/foodLogo.svg";
-import classes from "./ForkButton.module.css";
-
-function ForkButton() {
-  return <img src={logo} alt="Pulsante menu" className={classes.logo} />;
-}
-
-export default ForkButton;
